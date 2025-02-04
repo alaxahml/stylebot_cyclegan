e18b8ca9e02227f8d2443db28f86a55748afa9e1
alaxahml <45281963+alaxahml@users.noreply.github.com>
2025-02-04 15:50:48 +0300

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -12,6 +12,10 @@ class Config:
 
 
 def load_config(path=None) -> Config:
+    """
+    Функция загружает в класс Config необходимую информацию с окружения. Использовать её можно в боте, обращаясь к объекту класса Config, 
+    возвращенному с данной функции
+    """
     env = Env()
     env.read_env()
     return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
